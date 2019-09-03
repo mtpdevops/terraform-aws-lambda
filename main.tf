65f6ef73212341755c6cfd74e6538fc4f50c0671
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-09-03 13:42:31 +0530

    used static name for the iam policy

--- main.tf
+++ main.tf
@@ -125,8 +125,8 @@ data "aws_iam_policy_document" "invoke_function" {
 }
 
 resource "aws_iam_policy" "invoke_function" {
-  name_prefix = "${var.name}-invoke"
-  policy      = data.aws_iam_policy_document.invoke_function.json
+  name   = "${var.name}-invoke"
+  policy = data.aws_iam_policy_document.invoke_function.json
 }
 
 resource "aws_iam_role_policy_attachment" "invoke_function" {
